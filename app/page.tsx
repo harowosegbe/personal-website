9d9ffee2400a85ad53abe798ee09e0d2bd6c92ee
Hammed Arowosegbe <48373214+harowosegbe@users.noreply.github.com>
2024-09-20 21:30:22 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ import { getPosts } from "./get-posts";
 export const revalidate = 60;
 
 export default async function Home() {
+
+  return (
+    <h1>
+      Currently cooking, kindly check back later.
+    </h1>
+  )
   const posts = await getPosts();
   return <Posts posts={posts} />;
 }
