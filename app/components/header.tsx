3d2502edd6d25778c2e47cb792ce9af8012ce4de
harowosegbe <arowosegbe.hammed.olawale@gmail.com>
2026-02-04 09:02:29 +0000

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -24,8 +24,8 @@ export function Header() {
             target="_blank"
             className="inline-flex hover:bg-gray-200/50 dark:hover:bg-[#313131]/50 active:bg-gray-300/50 dark:active:bg-[#242424]/50 items-center px-3 py-2 rounded-md transition-colors whitespace-nowrap"
           >
-            <TweetIcon style={{ marginRight: 6 }} /> Follow{" "}
-            <span className="hidden md:inline">&nbsp;me</span>
+            <TweetIcon className="mr-0 md:mr-2" />
+            <span className="hidden md:inline">Follow me</span>
           </a>
           <a
             href="https://www.linkedin.com/in/hammed-arowosegbe/"
@@ -33,7 +33,8 @@ export function Header() {
             rel="noopener noreferrer"
             className="inline-flex hover:bg-gray-200/50 dark:hover:bg-[#313131]/50 active:bg-gray-300/50 dark:active:bg-[#242424]/50 items-center px-3 py-2 rounded-md transition-colors"
           >
-            LinkedIn
+            <LinkedInIcon className="mr-0 md:mr-2" />
+            <span className="hidden md:inline">LinkedIn</span>
           </a>
         </nav>
       </div>
@@ -57,6 +58,21 @@ function TweetIcon(props: any) {
         strokeWidth={1}
         d="M8.28 20.26c7.55 0 11.68-6.26 11.68-11.67v-.53c.8-.58 1.49-1.3 2.04-2.13-.74.33-1.53.54-2.36.65.85-.5 1.5-1.32 1.8-2.28-.78.48-1.66.81-2.6 1a4.1 4.1 0 0 0-7 3.74c-3.4-.17-6.43-1.8-8.46-4.29a4.1 4.1 0 0 0 1.28 5.48c-.68-.02-1.3-.2-1.86-.5v.05a4.11 4.11 0 0 0 3.29 4.02 4 4 0 0 1-1.85.08 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 2 18.43a11.67 11.67 0 0 0 6.28 1.83"
       />
+    </svg>
+  );
+}
+
+function LinkedInIcon(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
     </svg>
   );
 }
